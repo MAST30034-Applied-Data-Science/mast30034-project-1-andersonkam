a8d6fc215147e083f972355435a97ccc7bb00b08
andersonkam <andersonkam646@gmail.com>
2022-08-25 22:54:59 +1000

    added comments and explainations

--- scripts/download.py
+++ scripts/download.py
@@ -2,12 +2,10 @@
 # and modified by Un Leng Kam
 from urllib.request import urlretrieve
 import os
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-
 # data output directory is `data/raw/`
 output_relative_dir = '../data/'
 
+# data range
 YEARS = range(2016, 2018)
 MONTHS_2016 = range(1, 13)
 MONTHS_2017 = range(1, 6)
@@ -15,7 +13,7 @@ MONTHS_2017 = range(1, 6)
 # this is the URL template of the download destination
 URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_"
 
-
+# failsafe if directory does not exist.
 if not os.path.exists(output_relative_dir):
     os.makedirs(output_relative_dir)
 
@@ -23,7 +21,7 @@ target_dir = output_relative_dir + 'raw'
 if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 
-
+# download required dataset at specified range
 for month in MONTHS_2016:
     month = str(month).zfill(2)
     print(f"Begin year {2016} month {month}")
